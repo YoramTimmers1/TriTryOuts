ee82e06184f4b9ddd2efb14c5637870eaaa58b61
YoramTimmers <89836629+YoramTimmers@users.noreply.github.com>
2021-11-19 13:52:39 +0100

    klaar

---
+++ Tri-Tryouts3.py
@@ -0,0 +1,9 @@
+def NaamMaker():
+    while True:
+        Naam =input("Wat is je Naam? : ")
+        if Naam == "stop":
+            break
+        Leeftijd = input("Wat is je leeftijd? : ")
+        if Leeftijd == "stop":
+            break
+NaamMaker()
