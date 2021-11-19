bd2daf93a61cb001fc11ad72399fcee989c196cf
YoramTimmers <89836629+YoramTimmers@users.noreply.github.com>
2021-11-19 13:16:56 +0100

    tri

---
+++ Tri-Tryouts 1.py
@@ -0,0 +1,5 @@
+def TriTryout1(): # DEF
+    for i in range(7): #For loop
+        print("hallo wereld") #Print
+
+TriTryout1()   # Def Variable gebruiken om het terug te halen en te laten werken
---
+++ Tri-Tryouts2.py
@@ -0,0 +1,33 @@
+def addition(number1, number2):
+    total = number1 + number2
+    print(total)
+addition(10,12)
+    
+
+def subtraction(number1, number2):
+    total = number1 - number2
+    print(total)
+subtraction(58,34)
+
+def multiplication(number1, number2):
+    total = number1 * number2
+    print(total)
+multiplication(6, 7)
+
+def division(number1, number2):
+    total = number1 / number2
+    print(total)
+division(144, 12)
+
+def increment(number1, number2):
+    number2 == +1
+    total = number1 + number2
+    print(total)
+increment(12,1)
+
+
+def decrement(number1, number2):
+    number2 == -1
+    total = number1 - number2
+    print(total)
+decrement(31,1)
